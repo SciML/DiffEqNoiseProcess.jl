53ae6860c7db8a6358ecafd0f779531571a75f2c
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-20 09:11:37 -0700

    noise wrapper adaptive test

--- test/sde_noise_wrapper.jl
+++ test/sde_noise_wrapper.jl
@@ -43,3 +43,13 @@ prob2 = SDEProblem(f1,g1,ones(4),(0.0,1.0),noise=W3)
 
 dt = 1//2^(5)
 sol3 = solve(prob2,EM(),dt=dt)
+
+prob = SDEProblem(f1,g1,ones(2),(0.0,1.0))
+sol4 = solve(prob,SRI(),abstol=1e-8)
+
+W2 = NoiseWrapper(sol4.W)
+prob2 = SDEProblem(f1,g1,ones(2),(0.0,1.0),noise=W2)
+sol5 = solve(prob2,SRIW1(),abstol=1e-8)
+
+@test sol4.t != sol5.t
+@test ≈(sol5[end],sol4[end],atol=1e-1)
