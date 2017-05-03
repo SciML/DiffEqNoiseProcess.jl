ad0b2c8d13c96980898f810cbb1728b057e07199
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-03 15:58:07 -0700

    depwarn fix

--- src/correlated_noisefunc.jl
+++ src/correlated_noisefunc.jl
@@ -4,9 +4,9 @@ function construct_correlated_noisefunc(Γ)
   b = Vector{eltype(Γ)}(size(Γ,1))
   dist = function (W,dt)
     if typeof(W.dW) <: AbstractArray
-      return A*sqrt(abs(dt))*wiener_randn(size(W.dW))
+      return A*sqrt.(abs(dt))*wiener_randn(size(W.dW))
     else
-      return A*sqrt(abs(dt))*wiener_randn(typeof(W.dW))
+      return A*sqrt.(abs(dt))*wiener_randn(typeof(W.dW))
     end
   end
   bridge = function (W,W0,Wh,q,h)
@@ -18,11 +18,11 @@ CorrelatedWienerProcess(Γ,t0,W0,Z0=nothing) = NoiseProcess(t0,W0,Z0,construct_c
 
 function construct_correlated_noisefunc!(Γ)
   γ = svdfact(Γ)
-  A = γ[:U]*Diagonal(√γ[:S])
+  A = γ[:U]*Diagonal(√.(γ[:S]))
   b = Vector{eltype(Γ)}(size(Γ,1))
   dist = function (rand_vec,W,dt)
     wiener_randn!(b)
-    b .*= sqrt(abs(dt))
+    b .*= sqrt.(abs(dt))
     A_mul_B!(rand_vec,A,b)
   end
   bridge = function (rand_vec,W,W0,Wh,q,h)
@@ -31,19 +31,3 @@ function construct_correlated_noisefunc!(Γ)
   dist,bridge
 end
 CorrelatedWienerProcess!(Γ,t0,W0,Z0=nothing) = NoiseProcess(t0,W0,Z0,construct_correlated_noisefunc!(Γ)...,rswm=RSWM())
-
-"""
-construct_correlated_noisefunc(Γ::AbstractArray)
-
-Takes in a constant Covariance matrix Γ and spits out the noisefunc.
-"""
-function construct_correlated_noisefunc(Γ::AbstractArray)
-  γ = svdfact(Γ)
-  A = γ[:U]*Diagonal(√γ[:S])
-  b = Vector{eltype(Γ)}(size(Γ,1))
-  noise_func! = function (a,integrator)
-    randn!(b)
-    A_mul_B!(a,A,b)
-  end
-  NoiseProcess{:White,true,typeof(noise_func!)}(noise_func!)
-end
