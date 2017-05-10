6839ef1856dd96ebba8fbd5561d8bddb924ee69f
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-10 08:24:17 -0700

    make tests slightly easier

--- test/ornstein.jl
+++ test/ornstein.jl
@@ -13,9 +13,9 @@ u0 = 2.0
 expected_mean = μ + (u0 - μ)*exp(-Θ*t)
 expected_variance = (1-exp(-2Θ*t))*σ^2/(2Θ)
 monte_prob = MonteCarloProblem(prob;output_func = (sol,i)-> sol[end])
-sol = solve(monte_prob;dt=0.1,num_monte=1000000)
-@test abs(mean(sol) - expected_mean) < 0.4
-@test abs(var(sol) - expected_variance) < 0.4
+sol = solve(monte_prob;dt=0.1,num_monte=100)
+@test abs(mean(sol) - expected_mean) < 0.04
+@test abs(var(sol) - expected_variance) < 0.04
 
 W = OrnsteinUhlenbeckProcess!(Θ,μ,σ,0.0,ones(2),ones(2))
 prob = NoiseProblem(W,(0.0,1.0))
