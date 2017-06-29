12da4d398de85ad727c66c3d6fe2f2fa83ac7b59
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-06-29 08:26:49 -0700

    rng choice for Correlated

--- src/correlated_noisefunc.jl
+++ src/correlated_noisefunc.jl
@@ -2,32 +2,34 @@ function construct_correlated_noisefunc(Γ)
   γ = svdfact(Γ)
   A = γ[:U]*Diagonal(sqrt.(γ[:S]))
   b = Vector{eltype(Γ)}(size(Γ,1))
-  dist = function (W,dt)
+  dist = function (W,dt,rng)
     if typeof(W.dW) <: AbstractArray
-      return A*sqrt.(abs(dt))*wiener_randn(size(W.dW))
+      return A*sqrt.(abs(dt))*wiener_randn(rng,size(W.dW))
     else
-      return A*sqrt.(abs(dt))*wiener_randn(typeof(W.dW))
+      return A*sqrt.(abs(dt))*wiener_randn(rng,typeof(W.dW))
     end
   end
-  bridge = function (W,W0,Wh,q,h)
+  bridge = function (W,W0,Wh,q,h,rng)
     error("Bridging distribution is unknown. Cannot use adapativity")
   end
   dist,bridge
 end
-CorrelatedWienerProcess(Γ,t0,W0,Z0=nothing) = NoiseProcess(t0,W0,Z0,construct_correlated_noisefunc(Γ)...,rswm=RSWM())
+CorrelatedWienerProcess(Γ,t0,W0,Z0=nothing;rng = Xorshifts.Xoroshiro128Plus(rand(UInt64))) = NoiseProcess(t0,W0,Z0,construct_correlated_noisefunc(Γ)...,rswm=RSWM(),rng=rng)
 
 function construct_correlated_noisefunc!(Γ)
   γ = svdfact(Γ)
   A = γ[:U]*Diagonal(sqrt.(γ[:S]))
   b = Vector{eltype(Γ)}(size(Γ,1))
-  dist = function (rand_vec,W,dt)
-    wiener_randn!(b)
+  dist = function (rand_vec,W,dt,rng)
+    wiener_randn!(rng,b)
     b .*= sqrt.(abs(dt))
     A_mul_B!(rand_vec,A,b)
   end
-  bridge = function (rand_vec,W,W0,Wh,q,h)
+  bridge = function (rand_vec,W,W0,Wh,q,h,rng)
     error("Bridging distribution is unknown. Cannot use adapativity")
   end
   dist,bridge
 end
-CorrelatedWienerProcess!(Γ,t0,W0,Z0=nothing) = NoiseProcess(t0,W0,Z0,construct_correlated_noisefunc!(Γ)...,rswm=RSWM())
+CorrelatedWienerProcess!(Γ,t0,W0,Z0=nothing;rng = Xorshifts.Xoroshiro128Plus(rand(UInt64))) =
+                         NoiseProcess(t0,W0,Z0,construct_correlated_noisefunc!(Γ)...,
+                         rswm=RSWM(),rng = rng)
