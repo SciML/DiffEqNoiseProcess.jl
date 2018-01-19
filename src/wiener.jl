33ce150240704d48b2a804baa2f8fc3af3131221
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-19 10:27:39 -0800

    faster randn

--- src/wiener.jl
+++ src/wiener.jl
@@ -1,35 +1,30 @@
-one_over_sqrt2 = 1/sqrt(2)
-@inline wiener_randn() = randn()
-@inline wiener_randn(x...) = randn(x...)
-@inline wiener_randn!(x...) = randn!(x...)
-@inline wiener_randn{T<:Number}(::Type{Complex{T}}) = one_over_sqrt2*(randn(T)+im*randn(T))
-@inline wiener_randn{T<:Number}(::Type{Complex{T}},x...) = one_over_sqrt2*(randn(T,x...)+im*randn(T,x...))
-@inline wiener_randn{T<:Number}(y::AbstractRNG,::Type{Complex{T}},x...) = one_over_sqrt2*(randn(y,T,x...)+im*randn(y,T,x...))
-@inline wiener_randn{T<:Number}(y::AbstractRNG,::Type{Complex{T}}) = one_over_sqrt2*(randn(y,T)+im*randn(y,T))
-@inline function wiener_randn!{T<:Number}(y::AbstractRNG,x::AbstractArray{Complex{T}})
+const one_over_sqrt2 = 1/sqrt(2)
+@inline wiener_randn(rng,::Type{T}) where T = randn(rng,T)
+@inline wiener_randn(x,y) = randn(x,y)
+@inline wiener_randn!(rng,rand_vec) = randn!(rng,rand_vec)
+@inline wiener_randn(y::AbstractRNG,::Type{Complex{T}}) where T = one_over_sqrt2*(randn(y,T)+im*randn(y,T))
+
+@inline function wiener_randn!{T<:Number}(y::AbstractRNG,x::AbstractArray{<:Complex{T}})
   for i in eachindex(x)
     x[i] = one_over_sqrt2*(randn(y,T)+im*randn(y,T))
   end
 end
-@inline function wiener_randn!{T<:Number}(x::AbstractArray{Complex{T}})
-  for i in eachindex(x)
-    x[i] = one_over_sqrt2*(randn(T)+im*randn(T))
-  end
-end
 
-function WHITE_NOISE_DIST(W,dt,rng)
-  if typeof(W.dW) <: AbstractArray
+@inline function WHITE_NOISE_DIST(W,dt,rng)
+  if typeof(W.dW) <: AbstractArray && !(typeof(W.dW) <: SArray)
     return sqrt(abs(dt))*wiener_randn(rng,size(W.dW))
   else
     return sqrt(abs(dt))*wiener_randn(rng,typeof(W.dW))
   end
 end
+
 function WHITE_NOISE_BRIDGE(W,W0,Wh,q,h,rng)
   if typeof(W.dW) <: AbstractArray
     return sqrt((1-q)*q*abs(h))*wiener_randn(rng,size(W.dW))+q*Wh
   else
     return sqrt((1-q)*q*abs(h))*wiener_randn(rng,typeof(W.dW))+q*Wh
   end
+
 end
 WienerProcess(t0,W0,Z0=nothing;kwargs...) = NoiseProcess{false}(t0,W0,Z0,WHITE_NOISE_DIST,WHITE_NOISE_BRIDGE;kwargs...)
 
