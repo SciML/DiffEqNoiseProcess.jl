a8da7d8737c94788a744253cd1e5a2b3adcc470a
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-31 17:23:48 -0400

    ReverseDiff OOP support

--- src/init.jl
+++ src/init.jl
@@ -4,4 +4,10 @@ function __init__()
     @require CuArrays="3a865a2d-5b23-5a0f-bc46-62713ec82fae" begin
         wiener_randn!(rng::AbstractRNG,rand_vec::CuArrays.CuArray) = randn!(rand_vec)
     end
+
+    @require ReverseDiff="37e2e3b7-166d-5795-8a7a-e32c996b4267" begin
+        @inline function DiffEqNoiseProcess.wiener_randn(rng::Random.AbstractRNG,proto::ReverseDiff.TrackedArray)
+          ReverseDiff.track(convert.(eltype(proto.value),randn(rng,size(proto))))
+        end
+    end
 end
