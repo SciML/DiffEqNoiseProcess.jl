91c124311ac78826abcfddb74663ba0db998722f
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-11-23 04:55:02 -0500

    handle trackedreal noise

--- src/init.jl
+++ src/init.jl
@@ -13,8 +13,17 @@ function __init__()
         @inline function DiffEqNoiseProcess.wiener_randn(rng::Random.AbstractRNG,proto::ReverseDiff.TrackedArray)
           ReverseDiff.track(convert.(eltype(proto.value),randn(rng,size(proto))))
         end
+        @inline function DiffEqNoiseProcess.wiener_randn(rng::Random.AbstractRNG,proto::Array{<:ReverseDiff.TrackedReal})
+          ReverseDiff.track.(randn.(rng,eltype(DiffEqBase.value.(proto))))
+        end
         @inline function DiffEqNoiseProcess.wiener_randn(rng::Random.AbstractRNG,proto::AbstractArray{<:ReverseDiff.TrackedReal})
           ReverseDiff.track.(randn.(rng,eltype(DiffEqBase.value.(proto))))
+        end
+        @inline function DiffEqNoiseProcess.wiener_randn!(rng::AbstractRNG,rand_vec::Array{<:ReverseDiff.TrackedReal})
+            rand_vec .= ReverseDiff.track.(randn.((rng,),typeof.(DiffEqBase.value.(rand_vec))))
+        end
+        @inline function DiffEqNoiseProcess.wiener_randn!(rng::AbstractRNG,rand_vec::AbstractArray{<:ReverseDiff.TrackedReal})
+            rand_vec .= ReverseDiff.track.(randn.((rng,),typeof.(DiffEqBase.value.(rand_vec))))
         end
     end
 end
