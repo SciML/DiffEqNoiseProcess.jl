9974ffbda4b58e2dd5ec641c4beb953a302d13d5
Ricardo Rosa <rmsrosa@gmail.com>
2022-08-28 13:50:17 -0300

    test comparing NoiseFunction with NoiseTransport

--- test/noise_transport.jl
+++ test/noise_transport.jl
@@ -1,6 +1,7 @@
 @testset "NoiseTransport" begin
-    using DiffEqNoiseProcess, DiffEqBase, Distributions, Random, Test
+    using DiffEqNoiseProcess, Distributions, Random, Test
 
+    # test steps
     f = (u, p, t, Y) -> exp(Y * t)
 
     W = NoiseTransport(0.0, f, randn)
@@ -14,11 +15,23 @@
 
     @test W(W.curt + W.dt)[1] - W.curW == W.dW
 
+    # test solving it
     prob = NoiseProblem(W, (0.0, 1.0))
     sol = solve(prob; dt = 0.1)
 
     @test sol(nothing, nothing, sol.curt + sol.dt, sol.rv)[1] - sol.curW == sol.dW
 
+    # compare with NoiseFunction
+    f1 = (u, p, t, Y) -> exp(t)
+    f2 = (u, p, t) -> exp(t)
+    RV = (rng) -> nothing
+    W = NoiseTransport(0.0, f1, RV)
+    Y = NoiseFunction(0.0, f2)
+
+    tt = 0.0:0.1:1.0
+    @test W.(tt) == Y.(tt)
+
+    # system with scaler noise
     f = (out, u, p, t, Y) -> (out .= exp(Y * t))
     W = NoiseTransport(0.0, f, randn, 1.0, noise_prototype = rand(4))
     prob = NoiseProblem(W, (0.0, 1.0))
@@ -30,24 +43,27 @@
 
     @test out1 == sol(0.5)[1] == W(nothing, nothing, 0.5, sol.rv)[1]
 
-    f(u, p, t, rv) = sin(p[1] * t + rv[1]) + cos(p[2] * t + rv[2])
+    # scalar with random vector
+    f = (u, p, t, rv) -> sin(p[1] * t + rv[1]) + cos(p[2] * t + rv[2])
     t0 = 0.0
     rv = randn(2)
     p = (π, 2π)
     @test_nowarn NoiseTransport(t0, f, randn!, rv, noise_prototype = f(nothing, p, t0, rv))
 
-    f!(out, u, p, t, rv) = (out .= sin.(rv * t))
+    # system with random variable from Distributions.jl
+    f! = (out, u, p, t, rv) -> (out .= sin.(rv * t))
     RV(rng) = rand(rng, Beta(2, 3))
     @test_nowarn NoiseTransport(t0, f!, RV; noise_prototype = zeros(4))
 
-    function f!(out, u, p, t, v)
+    # system with random vector
+    f! = function (out, u, p, t, v)
         out[1] = sin(v[1] * t)
         out[2] = sin(t + v[2])
         out[3] = cos(t) * v[1] + sin(t) * v[2]
         nothing
     end
 
-    RV!(rng, v) = (v[1] = randn(rng); v[2] = rand(rng))
+    RV! = (rng, v) -> (v[1] = randn(rng); v[2] = rand(rng))
     rv = zeros(2)
 
     @test_nowarn NoiseTransport(t0, f!, RV!, rv, noise_prototype = zeros(3))
