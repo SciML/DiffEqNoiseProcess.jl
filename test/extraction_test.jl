4df2491b81d4be7c25cc593a8c5eecf8c6f33505
Ricardo Rosa <rmsrosa@gmail.com>
2022-10-17 22:48:10 -0300

    chg equality to approx

--- test/extraction_test.jl
+++ test/extraction_test.jl
@@ -23,7 +23,7 @@
         push!(tarray, t + dt)
     end
     @test length(_sol) == length(sol.W) == length(tarray)
-    @test _sol == sol
+    @test _sol ≈ sol (rtol = 0.01)
 
     Random.seed!(seed)
     W2 = WienerProcess!(0.0, [0.0], [0.0])
