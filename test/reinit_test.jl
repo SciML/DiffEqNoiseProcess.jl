62b3b31f0f74541aa19f3a2109a6bb26f9020654
Ricardo M. S. Rosa <rmsrosa@gmail.com>
2022-11-03 17:56:52 -0300

    finish reinit noisetranport test

--- test/reinit_test.jl
+++ test/reinit_test.jl
@@ -200,5 +200,22 @@
         varW = var(W.curW for i in 1:10_000 if reinit!(W, 0.0, t0 = t) === nothing)
         @test meanW≈expected_mean atol=0.1
         @test varW≈expected_variance rtol=0.1
+
+        prob = NoiseProblem(W, tspan)
+        ensemble_prob = EnsembleProblem(prob,
+                                        output_func = (sol, i) -> (first(sol(t)), false))
+        sol = solve(ensemble_prob, dt = 1 / 10, trajectories = 40_000)
+
+        @test mean(sol)≈expected_mean atol=0.1
+        @test var(sol)≈expected_variance atol=0.1
+
+        prob = SDEProblem(f, g, 1.0, tspan, noise = W, save_noise = true)
+
+        ensemble_probW = EnsembleProblem(prob,
+                                         output_func = (sol, i) -> (first(sol.W(t)), false))
+        solW_at_1 = solve(ensemble_probW, EM(), dt = 1 / 10, trajectories = 40_000)
+
+        @test mean(solW_at_1)≈expected_mean atol=0.1
+        @test var(solW_at_1)≈expected_variance atol=0.1
     end
 end
