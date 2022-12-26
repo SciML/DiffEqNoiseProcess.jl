2994f14ae2e50a1846147f42a21fd1297c981f27
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2022-12-26 11:18:44 +0530

    formattter

--- test/noise_wrapper.jl
+++ test/noise_wrapper.jl
@@ -273,13 +273,13 @@ end
     _seed = 0x952197dfddfdce1f
 
     if VERSION > v"1.6"
-        W = WienerProcess(t,rand_prototype,
-                        save_everystep=save_noise,
-                        rng = Xorshifts.Xoroshiro128Plus(_seed))
+        W = WienerProcess(t, rand_prototype,
+                          save_everystep = save_noise,
+                          rng = Xorshifts.Xoroshiro128Plus(_seed))
     else
-        W = WienerProcess(t,rand_prototype,
-                        save_everystep=save_noise,
-                        rng = MersenneTwisters.MT19937(_seed))
+        W = WienerProcess(t, rand_prototype,
+                          save_everystep = save_noise,
+                          rng = MersenneTwisters.MT19937(_seed))
     end
     bW = NoiseProcess(W, vec(W.dW))
     @test bW.dW isa AbstractVector && W.dW isa AbstractMatrix
