5f4b97cc1ecca9c55c6e3f9709662c35d4f2683b
oameye <orjan.ameye@hotmail.com>
2024-07-17 13:16:14 +0200

    add some tests

--- test/correlated.jl
+++ test/correlated.jl
@@ -2,6 +2,9 @@ using StaticArrays
 @testset "Correlated Wiener Process" begin
     using DiffEqNoiseProcess, DiffEqBase, Test, Statistics
 
+    W_not_correlated = WienerProcess(0.0, zeros(2), zeros(2))
+    @test W_not_correlated.covariance == nothing
+
     # oop
     ρ = 0.3
     Γ = [1 ρ; ρ 1]
@@ -33,7 +36,7 @@ using StaticArrays
     @time sol = Array(solve(ensemble_prob, dt = dt, trajectories = 1_000_000))
 
     @test zero(mean(sol, dims = 2)[:])≈mean(sol, dims = 2)[:] atol=1e-2
-    @test Γ≈cov(sol, dims = 2) / dt rtol=1e-2
+    @test W.Γ≈cov(sol, dims = 2) / dt rtol=1e-2
 
     # with StaticArrays
     Γ = @SMatrix [1.0 ρ
